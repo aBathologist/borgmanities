076c4711b5045115007f880b3bfda6758f4dadc0
abathologist <abathologist@gmail.com>
2015-01-19 00:43:47 -0500

    Still trying to work out the twitter stuff... Updating the (dead end progress) so far so I can branch and try another approach.

--- src/Web/Twitter.hs
+++ src/Web/Twitter.hs
@@ -1,19 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
+
 import Web.Twitter.Conduit
 import Web.Twitter.Types.Lens
 import Web.Authenticate.OAuth
+import Network.HTTP.Conduit
+import Data.Conduit
+import qualified Data.Conduit.List as CL
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Control.Monad.IO.Class
+import Control.Lens
+
 
 import Data.String.Conversions
 import Secrets
 
 tokens :: OAuth
 tokens = twitterOAuth
-    { oauthConsumerKey    = cs (public Secrets.twitterToken)
-    , oauthConsumerSecret = cs (secret Secrets.twitterToken)
+    { oauthConsumerKey    = cs $ public Secrets.twitterToken
+    , oauthConsumerSecret = cs $ secret Secrets.twitterToken
     }
 
 credentials :: Credential
 credentials = Credential
-    [ ("oauth_token",        cs (public Secrets.twitterKey))
-    , ("oauth_token_secret", cs (secret Secrets.twitterKey)) ]
+    [ ("oauth_token",        cs $ public Secrets.twitterKey)
+    , ("oauth_token_secret", cs $ secret Secrets.twitterKey) ]
 
+twInfo :: TWInfo
+twInfo = def
+    { twToken = def { twOAuth = tokens, twCredential = credentials }
+    , twProxy = Nothing
+    }
